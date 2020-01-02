c665ae432a63b1aaf83c0f0c837a69f1a9ebcf6b
ozgur <ozgurpir14@gmail.com>
2020-01-02 22:27:09 +0300

    project started

---
+++ src/main/java/com/grokonez/jwtauthentication/model/ActiveUser.java
@@ -0,0 +1,56 @@
+package com.grokonez.jwtauthentication.model;
+
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "active_users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "token"
+        })
+})
+public class ActiveUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String username;
+
+    @NotBlank
+    private String token;
+
+    public ActiveUser() {
+    }
+
+    public ActiveUser(String username, String token) {
+        this.username = username;
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/com/grokonez/jwtauthentication/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,67 @@
+package com.grokonez.jwtauthentication.security.services;
+
+import com.grokonez.jwtauthentication.model.ActiveUser;
+import com.grokonez.jwtauthentication.model.User;
+import com.grokonez.jwtauthentication.repository.ActiveUserRepository;
+import com.grokonez.jwtauthentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ActiveUserRepository activeUserRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+    	
+        User user = userRepository.findByUsername(username)
+                	.orElseThrow(() -> 
+                        new UsernameNotFoundException("User Not Found with -> username or email : " + username)
+        );
+
+        return UserPrinciple.build(user);
+    }
+
+    @Transactional
+    public boolean controlIfTokenExistsinDb(String token,
+                                         String username){
+        System.out.println("token is: " + token);
+        System.out.println("username is: " + username);
+
+        if(!activeUserRepository.existsByUsername(username)){
+            ActiveUser activeUser = new ActiveUser();
+            activeUser.setUsername(username);
+            activeUser.setToken(token);
+            activeUserRepository.save(activeUser);
+            return false;
+        } else{
+            activeUserRepository.deleteActiveUserByUsername(username);
+            return true;
+        }
+    }
+
+    @Transactional
+    public void saveActiveUser(ActiveUser activeUser){
+        activeUserRepository.save(activeUser);
+    }
+
+    @Transactional
+    public void deleteByUsername(String username){
+        activeUserRepository.deleteActiveUserByUsername(username);
+    }
+    @Transactional
+    public void deleteByToken(String token){
+        activeUserRepository.deleteActiveUserByToken(token);
+    }
+}
